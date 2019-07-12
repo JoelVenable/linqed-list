46e83292d25d8b2fa6256a27d58f734ae840452a
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-12 09:46:27 -0500

    Finished count of numbers

--- Program.cs
+++ Program.cs
@@ -62,6 +62,21 @@ namespace linqed_list
       //sortedNumbers.ToList().ForEach(number => System.Console.WriteLine(number));
 
 
+      // Output how many numbers are in this list
+      List<int> moreNumbers = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+
+
+      int countOfNumbers = moreNumbers.Count;
+      System.Console.WriteLine($"Count of numbers: {countOfNumbers}");
+
+
+
+
+
+
     }
   }
 }
